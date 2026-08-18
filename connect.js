438fcc152dac3e0fc5bd61ea88f39c7dae955b70
Chris Walker <chris@walker.group>
2026-08-18 18:10:39 +0000

    New connector opens snapshot pricing page, no Trello board handshake.

---
+++ connect.js
@@ -0,0 +1,22 @@
+/* global TrelloPowerUp */
+var HOST = "https://chr155-ai.github.io/walker-tenders-powerup";
+TrelloPowerUp.initialize({
+  "board-buttons": function (t) {
+    return [
+      {
+        text: "Pricing",
+        icon: {
+          dark: HOST + "/icon-dark.png",
+          light: HOST + "/icon-light.png",
+        },
+        callback: function (t) {
+          return t.modal({
+            url: HOST + "/pricing.html",
+            fullscreen: true,
+            title: "Walker Pricing",
+          });
+        },
+      },
+    ];
+  },
+});
